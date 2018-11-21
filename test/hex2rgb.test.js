d9a9813d8be2ae63f12a16a8331cf852030d9b33
dmkon <dmkon@hotmail.com>
2018-11-21 15:56:25 -0500

    Lesson 18 end - Sinon spies

--- test/hex2rgb.test.js
+++ test/hex2rgb.test.js
@@ -1,37 +1,81 @@
 var hex2rgb = require("../lib/hex2rgb");
-var assert = require("assert");
-var should = require("chai").should();
 var expect = require("chai").expect;
 var sinon = require("sinon");
 
-describe.only("hex2rgb", function() {
-    it("should return an error if the value is not a hex code", function(done) {
+describe.only("hex2rgb", function () {
 
-        hex2rgb.convert("blue", function (error, result){
-            
-            //assert(error);
+  describe("convert method", function () {
 
-            //error.should.exist;
+    it("should call parse once", function (done) {
 
-            expect(error).to.exist;
+      sinon.spy(hex2rgb, "parse");
 
-            done();
-        });
+      hex2rgb.convert("#ffffff", function (err, result) {
+
+        expect(hex2rgb.parse.calledOnce).to.be.true;
+        expect(hex2rgb.parse.args[0][0]).to.have.length(6);
+
+        hex2rgb.parse.restore();
+        done();
+
+      });
+
     });
 
-    it("should return a correctly converted rgb value", function(done) {
-        hex2rgb.convert("#fff", function(error, result){
-            
-            //assert.strictEqual(error, null);
-            //assert.deepEqual(result, [255, 255, 255]);
-            
-            //should.not.exist(error);
-            //result.should.deep.equal( [255, 255, 255] )
+    it("should always return the result of parse", function (done) {
 
-            expect(error).to.not.exist;
-            expect(result).to.deep.equal( [255, 255, 255] )
-            
-            done();
-        });
+      sinon.stub(hex2rgb, "parse").returns([0,900,100]);
+
+      hex2rgb.convert("#abc", function (error, result) {
+
+        expect(result).to.deep.equal([0,900,100]);
+
+        hex2rgb.parse.restore();
+        done();
+
+      });
+
     });
+
+    it("should always pass a 6 item array to parse", function (done) {
+
+      var mock = sinon.mock(hex2rgb);
+      mock.expects("parse").twice().withExactArgs("000000".split(''));
+
+      hex2rgb.convert("#000000", function (error, result) {
+
+        hex2rgb.convert("#000", function (error, result) {
+
+          mock.verify();
+          done();
+
+        });
+
+      });
+
+    });
+
+    it("should return an error if the value is not a hex code", function (done) {
+
+      hex2rgb.convert("blue", function (error, result) {
+        expect(error).to.exist;
+        done();
+      });
+
+    });
+
+    it("should return a correctly converted rgb value", function (done) {
+
+      hex2rgb.convert("#fff", function (error, result) {
+
+        expect(error).to.not.exist;
+        expect(result).to.deep.equal([255, 255, 255]);
+
+        done();
+      });
+
+    });
+
+  });
+
 });

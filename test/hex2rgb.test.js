1db8ddbcaa50b8b1617b6414e6e49ee79146ae80
dmkon <dmkon@hotmail.com>
2018-11-21 10:20:28 -0500

    Lesson 7 end - testing async functions

--- test/hex2rgb.test.js
+++ test/hex2rgb.test.js
@@ -2,15 +2,19 @@ var hex2rgb = require("../lib/hex2rgb");
 var assert = require("assert");
 
 describe("hex2rgb", function() {
-    it("should throw an error if the value is not a hex code", function() {
+    it("should return an error if the value is not a hex code", function(done) {
 
-        assert.throws(function() {
-            hex2rgb("blue");
-        }, /Invalid hexadecimal string/);
+        hex2rgb("blue", function (error, result){
+            assert(error);
+            done();
+        });
     });
 
-    it("should return a correctly converted rgb value", function() {
-        var rgb = hex2rgb("#fff");
-        assert.deepEqual(rgb, [255, 255, 255]);
+    it("should return a correctly converted rgb value", function(done) {
+        hex2rgb("#fff", function(error, result){
+            assert.strictEqual(error, null);
+            assert.deepEqual(result, [255, 255, 255]);
+            done();
+        });
     })
 });

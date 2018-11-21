f0043672991db98db00a9efc43380792298dd139
dmkon <dmkon@hotmail.com>
2018-11-21 11:45:27 -0500

    Lesson 11 end - chai - assert

--- test/getPalette.test.js
+++ test/getPalette.test.js
@@ -1,9 +1,10 @@
 var getPalette = require("../lib/getPalette");
 var expect = require("chai").expect;
+var assert = require("chai").assert;
 
 describe("getPalette", function() {
 
-    it.only("should throw an error if the result is not an array", function(done) {
+    it("should throw an error if the result is not an array", function(done) {
 
         var notArray = function() {
             getPalette(process.cwd() + "/test/fixtures/config-palette-non-array.json");
@@ -13,8 +14,11 @@ describe("getPalette", function() {
         done();
     });
     
-    it("should return an array with 3 items", function() {
+    it.only("should return an array with 3 items", function() {
         var palette = getPalette();
         expect(palette).to.be.an("array").with.length(3);
+
+        assert.isArray(palette, "did not return an array");
+        assert.lengthOf(palette, 3, "did not return 3 items");
     });
 });

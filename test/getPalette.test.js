3f2fd4e82c380e8e08a3544ba73bd35b0825bc7a
dmkon <dmkon@hotmail.com>
2018-11-21 08:50:09 -0500

    Lesson 4 end

--- test/getPalette.test.js
+++ test/getPalette.test.js
@@ -2,6 +2,10 @@ var getPalette = require("../lib/getPalette");
 var assert = require("assert");
 
 describe("getPalette", function() {
+    it("should throw an error if the result is not an array", function() {
+        assert.throws(getPalette, /is not an array/)
+    });
+    
     it("should return an array with 3 items", function() {
         var palette = getPalette();
         assert(Array.isArray(palette), "did not return an array");
